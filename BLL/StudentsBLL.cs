960ef9e0c95ee5f83a2068f6a3ae932698cd0445
Kleyton72 <jeanc.1582@gmail.com>
2021-06-06 16:49:19 -0400

    Agregando BLL de estudiantes

---
+++ BLL/StudentsBLL.cs
@@ -0,0 +1,46 @@
+﻿using EFCoreTutorials.DAL;
+using EFCoreTutorials.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorials.BLL
+{
+    public class StudentsBLL
+    {
+
+        public List<Students> StudentsByName(string name)
+        {
+            var context = new SchoolContext();
+            var students = context.Students
+                            .Where(s => s.FirstName == name)
+                            .ToList();
+
+            return students;
+        }
+
+        public Students FirstStudentsByName(string name)
+        {
+            var context = new SchoolContext();
+            var student = context.Students
+                            .Where(s => s.FirstName == name)
+                            .Include(s => s.Grade)
+                            .FirstOrDefault();
+
+            return student;
+        }
+
+        public Students FirstStudentsByNameRawQuery(string name)
+        {
+            var context = new SchoolContext();
+            var student = context.Students
+                            .FromSqlRaw($"SELECT * FROM Students WHERE FirstName = '{name}'")
+                            .Include(s => s.Grade)
+                            .FirstOrDefault();
+
+            return student;
+        }
+    }
+}
